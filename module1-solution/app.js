bf157b20c6704a33150a869f4ed4db8191dc9c03
Philip Riley <pr@cetrea.com>
2016-12-11 22:46:03 +0100

    final submission

--- module1-solution/app.js
+++
@@ -1,7 +0,0 @@
-(function () {
-'use strict';
-
-angular.module('LunchCheck', [])
-  .controller('LunchCheckController', ['$scope', Ctrl]);
-
-})();
---
+++ module1-solution/js/app.js
@@ -0,0 +1,33 @@
+(function () {
+  'use strict';
+
+  //angular.module('LunchCheck', []);
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  var getLengthOfItems = function (unparsedItems) {
+    var items = "";
+    if (unparsedItems.length > 0)
+      items = unparsedItems.split(",");
+    return items.length;
+  };
+
+  function LunchCheckController($scope) {
+    $scope.items = "";
+    $scope.message = "";
+    $scope.checkIfTooMuch = function () {
+
+      var len = getLengthOfItems($scope.items);
+
+      if (len < 1) {
+        $scope.message = "Please enter data first";
+      } else if (len > 0 && len <= 3) {
+        $scope.message = "Enjoy";
+      } else {
+        $scope.message = "Too much!!";
+      }
+    };
+  };
+})();
